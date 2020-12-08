518264027e3a6e73099f338fdc5779610cdabf0e
MK1499 <m.khaled1499@gmail.com>
2020-12-08 20:24:17 +0200

    add teachers screen

--- projects/quran-web/src/app/app-routing.module.ts
+++ projects/quran-web/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { TeacherComponent } from './Screens/teacher/teacher.component';
+import { CarouselComponent } from './Components/carousel/carousel.component';
+import { HomeComponent } from './Screens/home/home.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Screens/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent,children:[
+    {path:'',component:CarouselComponent},
+    {path:'teachers',component:TeacherComponent}
+  ]},
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- projects/quran-web/src/app/app.module.ts
+++ projects/quran-web/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Screens/login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Screens/home/home.component';
+import { CarouselComponent } from './Components/carousel/carousel.component';
+import { TeacherComponent } from './Screens/teacher/teacher.component';
+import { TeacherCardComponent } from './Components/teacher-card/teacher-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent,
+    CarouselComponent,
+    TeacherComponent,
+    TeacherCardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
